08b670e74d2013d1c6a0af2090f873b9d4cd4b5e
Adarsh Shah <github@shahadarsh.com>
2023-04-24 17:16:27 -0400

    format json

--- conductor/src/lib.rs
+++ conductor/src/lib.rs
@@ -197,59 +197,59 @@ pub async fn create_networkpolicy(client: Client, name: &str) -> Result<(), Cond
     let np_api: Api<NetworkPolicy> = Api::namespaced(client, name);
     let params: PatchParams = PatchParams::apply("conductor").force();
     let np = serde_json::json!({
-				"apiVersion": "networking.k8s.io/v1",
-				"kind": "NetworkPolicy",
-				"metadata": {
-						"name": format!("{name}"),
-						"namespace": format!("{name}"),
-				},
-				"spec": {
-						"podSelector": {},
-						"policyTypes": [
-								"Egress"
-						],
-						"egress": [
-								{
-										"to": [
-												{
-														"namespaceSelector": {
-																"matchLabels": {
-																		"kubernetes.io/metadata.name": "kube-system"
-																}
-														}
-												},
-												{
-														"podSelector": {
-																"matchLabels": {
-																		"k8s-app": "kube-dns"
-																}
-														}
-												}
-										],
-										"ports": [
-												{
-														"protocol": "UDP",
-														"port": 53
-												}
-										]
-								},
-								{
-										"to": [
-												{
-														"ipBlock": {
-																"cidr": "0.0.0.0/0",
-																"except": [
-																		"10.0.0.0/8",
-																		"172.16.0.0/12",
-																		"192.168.0.0/16"
-																]
-														}
-												}
-										]
-								}
-						]
-				}
-		});
+            "apiVersion": "networking.k8s.io/v1",
+            "kind": "NetworkPolicy",
+            "metadata": {
+                    "name": format!("{name}"),
+                    "namespace": format!("{name}"),
+            },
+            "spec": {
+                    "podSelector": {},
+                    "policyTypes": [
+                            "Egress"
+                    ],
+                    "egress": [
+                            {
+                                    "to": [
+                                            {
+                                                    "namespaceSelector": {
+                                                            "matchLabels": {
+                                                                    "kubernetes.io/metadata.name": "kube-system"
+                                                            }
+                                                    }
+                                            },
+                                            {
+                                                    "podSelector": {
+                                                            "matchLabels": {
+                                                                    "k8s-app": "kube-dns"
+                                                            }
+                                                    }
+                                            }
+                                    ],
+                                    "ports": [
+                                            {
+                                                    "protocol": "UDP",
+                                                    "port": 53
+                                            }
+                                    ]
+                            },
+                            {
+                                    "to": [
+                                            {
+                                                    "ipBlock": {
+                                                            "cidr": "0.0.0.0/0",
+                                                            "except": [
+                                                                    "10.0.0.0/8",
+                                                                    "172.16.0.0/12",
+                                                                    "192.168.0.0/16"
+                                                            ]
+                                                    }
+                                            }
+                                    ]
+                            }
+                    ]
+            }
+    });
 
     info!("\nCreating Network Policy {} if it does not exist", name);
     let _o: NetworkPolicy = np_api
