0f5d39675c98bebfa715c3b38abcb24a8d566c9d
Steven Miller <sjmiller609@gmail.com>
2023-06-16 11:40:05 -0400

    Update dataplane-webserver/src/routes/metrics.rs

--- dataplane-webserver/src/routes/metrics.rs
+++ dataplane-webserver/src/routes/metrics.rs
@@ -81,8 +81,6 @@ impl ExprVisitor for NamespaceVisitor {
     }
 }
 
-// ("Authorization" = String, Header, example = "Bearer eyJhbGc....rGD0SQ", description = "JWT bearer token in Authorization header")
-
 #[utoipa::path(
     context_path = "/{namespace}/metrics",
     params(
