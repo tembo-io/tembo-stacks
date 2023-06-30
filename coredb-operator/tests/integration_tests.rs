7f01863c65571f5db4c07c99e09847ceea4bd838
Nick Hudson <nick.hudson@gmail.com>
2023-06-30 09:51:34 -0500

    more race condition fixes

--- coredb-operator/tests/integration_tests.rs
+++ coredb-operator/tests/integration_tests.rs
@@ -978,7 +978,7 @@ mod test {
         let coredb_resource = coredbs.patch(name, &params, &patch).await.unwrap();
 
         // give it time to stop
-        thread::sleep(Duration::from_millis(8000));
+        thread::sleep(Duration::from_millis(30000));
 
         // pod must not be ready
         let res = coredb_resource.primary_pod(client.clone()).await;
