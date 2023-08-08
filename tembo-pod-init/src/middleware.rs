46ae79a25c0e631d73e7bc9e7f8a04ea1eb0dc6a
Nick Hudson <nick.hudson@gmail.com>
2023-08-08 14:33:01 -0500

    attempt to not log health

--- tembo-pod-init/src/middleware.rs
+++ tembo-pod-init/src/middleware.rs
@@ -11,13 +11,13 @@ pub struct CustomLevelRootSpanBuilder;
 
 impl RootSpanBuilder for CustomLevelRootSpanBuilder {
     fn on_request_start(request: &ServiceRequest) -> Span {
-        // If the path matches our excluded routes, set the log level to ERROR (to effectively mute it)
-        // Else, set the log level to INFO.
-        let level = match request.path() {
-            "/health/liveness" | "/health/readiness" => Level::ERROR,
-            _ => Level::INFO,
-        };
-        tracing_actix_web::root_span!(level = level, request)
+        if ["/health/liveness", "/health/readiness"].contains(&request.path()) {
+            // Don't create a span for the specified paths
+            return Span::none();
+        }
+
+        // For all other paths, create a span with INFO level
+        tracing_actix_web::root_span!(level = Level::INFO, request)
     }
 
     fn on_request_end<B: MessageBody>(span: Span, outcome: &Result<ServiceResponse<B>, Error>) {
